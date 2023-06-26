7399dff539073d605b4afe02190cfb1e0c92db56
bufaonanet <douglas.bufaonanet@outlook.com>
2023-06-26 15:23:59 -0300

    concluindo docker-compose dos projetos

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,8 @@
 
 using Basket.API.Repositories.Interfaces;
 using Basket.API.Repositories;
+using Basket.API.GrpcServices;
+using Discount.gRPC.Protos;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,8 +12,15 @@ builder.Services.AddStackExchangeRedisCache(options =>
     options.Configuration = builder.Configuration
     .GetValue<string>("CacheSettings:ConnectionString");
 });
+// Grpc Configuration
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
+            (o => o.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]));
+
+builder.Services.AddScoped<DiscountGrpcService>();
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
 builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
