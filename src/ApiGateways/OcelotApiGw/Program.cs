d08d07bc5992e5318a949c07b66cfb45e03b2641
bufaonanet <douglas.bufaonanet@outlook.com>
2023-06-30 16:19:01 -0300

    adicionando ocelot ApiGateway

---
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,33 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Host
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        config.AddJsonFile(
+            $"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", 
+            optional: true, 
+            reloadOnChange: true);
+    })
+    .ConfigureLogging((hostingContext,loggingBuilder) =>
+    {
+        loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+        loggingBuilder.AddConsole();
+        loggingBuilder.AddDebug();
+    });
+
+builder.Services
+    .AddOcelot()
+    .AddCacheManager(settings => settings.WithDictionaryHandle()); ;
+
+var app = builder.Build();
+
+await app.UseOcelot();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
